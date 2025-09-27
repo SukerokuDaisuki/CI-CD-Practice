e7588f1128fbdf53e992772ebbdd41e1b7e6015d
KaitoNishizawa <nishizawa.kaito@g.sp.m.is.nagoya-u.ac.jp>
2025-09-27 19:55:29 +0900

    引き算のテストを追加

--- test_main.py
+++ test_main.py
@@ -4,6 +4,9 @@ from main import add, multiply, divide
 def test_add():
     assert add(2, 3) == 5
 
+def test_sub():
+    assert sub(2, 3) == -1
+
 def test_multiply():
     assert multiply(2, 3) == 6
 
